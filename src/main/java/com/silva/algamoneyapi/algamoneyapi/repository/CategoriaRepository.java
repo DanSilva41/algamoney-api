c1e2aa9bd6f3b40602f2123cbaa3e6397411ffe3
Danilo da Silva Pereira <dansilva41@hotmail.com>
2018-11-24 11:25:22 -0200

    Consultando primeiro recuso criado (/categorias)

---
+++ src/main/java/com/silva/algamoneyapi/algamoneyapi/repository/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package com.silva.algamoneyapi.algamoneyapi.repository;
+
+import com.silva.algamoneyapi.algamoneyapi.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+}
