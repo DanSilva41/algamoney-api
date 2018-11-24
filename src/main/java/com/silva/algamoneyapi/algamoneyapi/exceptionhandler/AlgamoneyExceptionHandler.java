ed864456c46106316a54a81c9ecfd9632522492c
Danilo da Silva Pereira <dansilva41@hotmail.com>
2018-11-24 13:44:09 -0200

    Validando vários erros com Bean Validation

--- src/main/java/com/silva/algamoneyapi/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/silva/algamoneyapi/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -7,9 +7,15 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @ControllerAdvice
 public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
@@ -17,11 +23,54 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
     @Autowired
     private MessageSource messageSource; // possível ler arquivos [messages.properties]
 
+    /**
+     * Serve para tratar exceções lançadas quando não foi possível ler
+     * o corpo enviado na requisição
+     *
+     * @param ex
+     * @param headers
+     * @param status
+     * @param request
+     * @return ResponseEntity<Object>
+     */
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
         String mensagemDesenvolvedor = ex.getCause().toString();
-        return handleExceptionInternal(ex, new Erro(mensagemUsuario, mensagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
+        List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+        return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    /**
+     * Serve para tratar exceções lançadas quando o argumento enviada para método(recurso)
+     * não é válido - (@Valid)
+     *
+     * @param ex
+     * @param headers
+     * @param status
+     * @param request
+     * @return ResponseEntity<Object>
+     */
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<Erro> erros = criarListaDeErros(ex.getBindingResult());
+        return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    /**
+     * @param bindingResult objeto que contém lista de erros por validação
+     * @return List<Erro>
+     */
+    private List<Erro> criarListaDeErros(BindingResult bindingResult) {
+        List<Erro> erros = new ArrayList<>(0);
+
+        bindingResult.getFieldErrors().forEach(fieldError -> {
+            String mensagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+            String mensagemDesenvolvedor = fieldError.toString();
+            erros.add(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+        });
+        return erros;
     }
 
     public static class Erro {
--- src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -1,6 +1,6 @@
 CREATE TABLE categoria (
 	codigo BIGINT(20) AUTO_INCREMENT PRIMARY KEY,
-	nome VARCHAR(50) NOT NULL
+	nome VARCHAR(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO categoria (nome) values ('Lazer');
